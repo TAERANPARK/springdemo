f2bfec8eaea274738338ca637b8a976c21939df9
Park Tae Ran\taera <taeran0129@naver.com>
2025-09-08 10:43:46 +0900

    spring

--- src/main/java/com/multi/controller/HomeController.java
+++ src/main/java/com/multi/controller/HomeController.java
@@ -5,11 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
     @GetMapping({"/", "/home"})
     public String home(Model model){
         model.addAttribute("msg", "Spring Legacy (XML) running!");
         return "home"; // /WEB-INF/views/home.jsp
+    }
+
+    @GetMapping("/message")
+    public String message(Model model){
+        model.addAttribute("msg", "Hello Spring");
+        return "show";
+    }
+
+    @GetMapping("/message")
+    public String msg(Model model){
+        return "hi";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,9 @@
 (선택) src/main/webapp/index.jsp 는 단순 리다이렉트:
 <%@ page contentType="text/html; charset=UTF-8" %>
 <%
+    // request.getContextPath() 루트 경로를 반환
+    //response.sendRedirect(/home) 요청을 한다는 것 자체가 service를 의미한다
+
     response.sendRedirect(request.getContextPath() + "/home");
 %>
 
